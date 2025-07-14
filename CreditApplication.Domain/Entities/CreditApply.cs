3ff050b3f33bc42ff9a3b5a2ab87cdf6b7182d77
tiagoggdev <santiagogarciagarzon.8@gmail.com>
2025-07-14 14:33:38 -0500

    Domain layer with Entities added

---
+++ CreditApplication.Domain/Entities/CreditApply.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditApplication.Domain.Entities
+{
+    public class CreditApply
+    {
+        public int Id { get; set; }
+        public bool State {  get; set; }
+        public DateTime CreateDate { get; set; }
+        public int IdClient { get; set; }
+        public Client Client { get; set; } = null!;
+
+    }
+}
