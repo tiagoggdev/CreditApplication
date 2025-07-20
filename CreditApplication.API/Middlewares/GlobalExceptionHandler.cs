7051de1d78bfae8ba408f0533265602d07391ace
tiagoggdev <santiagogarciagarzon.8@gmail.com>
2025-07-20 11:23:43 -0500

    feat(core): Middleware for global exceptions has been implemented

---
+++ CreditApplication.API/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditApplication.API.Middlewares
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            int status = exception switch
+            {
+                ArgumentException => StatusCodes.Status400BadRequest,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = status,
+                Title = "An error occurred",
+                Type = exception.GetType().Name,
+                Detail = exception.Message
+            };
+
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+            return true;
+
+        }
+    }
+}
--- CreditApplication.API/Program.cs
+++ CreditApplication.API/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using CreditApplication.API.Middlewares;
 using CreditApplication.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,14 +10,28 @@ var connectionString =
     builder.Configuration.GetConnectionString("DefaultConnection")
         ?? throw new InvalidOperationException("Connection string"
         + "'DefaultConnection' not found.");
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+//Global Exception Errors I
+builder.Services.AddProblemDetails();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+//MediatR
+var assemblies = Assembly.Load("CreditApplication.Application");
+
+builder.Services.AddMediatR(cfg => {
+    cfg.RegisterServicesFromAssembly(assemblies);
+});
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+//Global Exception Errors II
+app.UseExceptionHandler();
+app.UseStatusCodePages();
 
 
 if (app.Environment.IsDevelopment())
