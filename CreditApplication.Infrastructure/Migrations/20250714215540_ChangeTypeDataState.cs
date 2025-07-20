2d4fe81e8bd7bfb7eae0d9042c168c8d1bbe6bf9
tiagoggdev <santiagogarciagarzon.8@gmail.com>
2025-07-20 11:11:49 -0500

    Typedata of the State parameter has changed

---
+++ CreditApplication.Infrastructure/Migrations/20250714215540_ChangeTypeDataState.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CreditApplication.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTypeDataState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "State",
+                table: "CreditApplications",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "State",
+                table: "CreditApplications",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
