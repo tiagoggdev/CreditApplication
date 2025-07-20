99d9ebb7ea48ed5dd4ceb97faa3f2eff17cf4d79
tiagoggdev <santiagogarciagarzon.8@gmail.com>
2025-07-20 11:24:29 -0500

    feat(controllers): ClientController and CreditApplicationController using MediatR pattern has been added

---
+++ CreditApplication.API/Controllers/ClientController.cs
@@ -0,0 +1,33 @@
+﻿using CreditApplication.Application.UseCases.Commands.CreateClientCommand;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditApplication.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ClientController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateClient(CreateClientCommand command)
+        {
+            if (command == null) return BadRequest();
+
+            var response = await _mediator.Send(command);
+
+            if (response.IsSuccess)
+            {
+                return Ok(new { message = "Client created successfully" });
+            }
+
+            return BadRequest(response);
+        }
+    }
+}
---
+++ CreditApplication.API/Controllers/CreditApplicationController.cs
@@ -0,0 +1,49 @@
+﻿using CreditApplication.Application.UseCases.Commands.CreateApplicationCommand;
+using CreditApplication.Application.UseCases.Queries.GetApplicationByClientQuery;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditApplication.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CreditApplicationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CreditApplicationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetApplies")]
+        public async Task<IActionResult> GetApplies([FromQuery] int clientId)
+        {
+            if (clientId == 0) return BadRequest();
+
+            var result = await _mediator.Send(new GetApplicationByClientQuery { IdClient = clientId });
+
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateApplies(CreateApplicationCommand command)
+        {
+            if (command == null) return BadRequest();
+
+            var result = await _mediator.Send(command);
+
+            if (result.IsSuccess)
+            {
+                return Ok(new {message = "Application created successfully" });
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
