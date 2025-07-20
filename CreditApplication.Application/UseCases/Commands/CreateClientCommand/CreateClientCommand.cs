3cd2535f75fa3b94e65285e7c091b7f1fcdd50d5
tiagoggdev <santiagogarciagarzon.8@gmail.com>
2025-07-20 11:17:41 -0500

    feat(customer): command and handler for customer has been implemented

---
+++ CreditApplication.Application/UseCases/Commands/CreateClientCommand/CreateClientCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CreditApplication.Application.Common.Result;
+using MediatR;
+
+namespace CreditApplication.Application.UseCases.Commands.CreateClientCommand
+{
+    public class CreateClientCommand : IRequest<Result<bool>>
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+    }
+}
---
+++ CreditApplication.Application/UseCases/Commands/CreateClientCommand/CreateClientHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CreditApplication.Application.Common.Result;
+using CreditApplication.Domain.Entities;
+using CreditApplication.Infrastructure.Data;
+using MediatR;
+
+namespace CreditApplication.Application.UseCases.Commands.CreateClientCommand
+{
+    public class CreateClientHandler : IRequestHandler<CreateClientCommand, Result<bool>>
+    {
+        private readonly AppDbContext _context;
+
+        public CreateClientHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<bool>> Handle(CreateClientCommand request, CancellationToken cancellationToken)
+        {
+            var clientToCreate = new Client
+            {
+                Name = request.Name,
+                Email = request.Email,
+                SignUpDate = DateTime.Now
+            };
+
+            _context.Clients.Add(clientToCreate);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Result<bool>.Ok(true);
+        }
+    }
+}
