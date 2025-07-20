081e04f1a945d80f69ed30f7c5679ebc7b114ad5
tiagoggdev <santiagogarciagarzon.8@gmail.com>
2025-07-20 11:14:01 -0500

    refactor: StatusEnum has been created and added in CreditApply

--- CreditApplication.Domain/Entities/CreditApply.cs
+++ CreditApplication.Domain/Entities/CreditApply.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CreditApplication.Domain.Enums;
 
 namespace CreditApplication.Domain.Entities
 {
     public class CreditApply
     {
         public int Id { get; set; }
-        public bool State {  get; set; }
+        public StatusEnum State {  get; set; }
         public DateTime CreateDate { get; set; }
         public int IdClient { get; set; }
         public Client Client { get; set; } = null!;
