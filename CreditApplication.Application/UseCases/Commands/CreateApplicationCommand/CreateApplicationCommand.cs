5586fd4b6595792b70f691cdf920caabf74468b0
tiagoggdev <santiagogarciagarzon.8@gmail.com>
2025-07-20 11:17:00 -0500

    feat(application): command and handler for application has been implemented

---
+++ CreditApplication.Application/UseCases/Commands/CreateApplicationCommand/CreateApplicationCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CreditApplication.Application.Common.Result;
+using CreditApplication.Domain.Enums;
+using MediatR;
+
+namespace CreditApplication.Application.UseCases.Commands.CreateApplicationCommand
+{
+    public class CreateApplicationCommand : IRequest<Result<bool>>
+    {
+        public StatusEnum Status { get; set; }
+        public int IdClient { get; set; }
+    }
+}
---
+++ CreditApplication.Application/UseCases/Commands/CreateApplicationCommand/CreateApplicationHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using CreditApplication.Application.Common.Result;
+using CreditApplication.Domain.Entities;
+using CreditApplication.Infrastructure.Data;
+using MediatR;
+
+namespace CreditApplication.Application.UseCases.Commands.CreateApplicationCommand
+{
+    public class CreateApplicationHandler : IRequestHandler<CreateApplicationCommand, Result<bool>>
+    {
+        private readonly AppDbContext _context;
+
+        public CreateApplicationHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<bool>> Handle(CreateApplicationCommand request, CancellationToken cancellationToken)
+        {
+            var appl = new CreditApply
+            {
+                State = request.Status,
+                CreateDate = DateTime.Now,
+                IdClient = request.IdClient
+            };
+
+            _context.CreditApplications.Add(appl);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Result<bool>.Ok(true);
+        }
+    }
+}
