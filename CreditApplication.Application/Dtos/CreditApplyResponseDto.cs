0f7e2640b5e20fdc931d6aa6753c0cc023f7e91b
tiagoggdev <santiagogarciagarzon.8@gmail.com>
2025-07-20 11:15:21 -0500

    feat(core): Result pattern has been added and CreditApplyResponseDto has created

---
+++ CreditApplication.Application/Dtos/CreditApplyResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CreditApplication.Domain.Enums;
+
+namespace CreditApplication.Application.Dtos
+{
+    public record CreditApplyResponseDto(StatusEnum State, DateTime CreateDate);
+}
