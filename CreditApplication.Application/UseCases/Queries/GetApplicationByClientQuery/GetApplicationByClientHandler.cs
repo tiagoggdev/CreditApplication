37bd5e2b41227e2ade0a0111d60938e7dd53d5e5
tiagoggdev <santiagogarciagarzon.8@gmail.com>
2025-07-20 11:18:05 -0500

    feat(customer): query for customer by clientId has been implemented

---
+++ CreditApplication.Application/UseCases/Queries/GetApplicationByClientQuery/GetApplicationByClientHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CreditApplication.Application.Common.Result;
+using CreditApplication.Application.Dtos;
+using CreditApplication.Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditApplication.Application.UseCases.Queries.GetApplicationByClientQuery
+{
+    public class GetApplicationByClientHandler : IRequestHandler<GetApplicationByClientQuery, Result<ICollection<CreditApplyResponseDto>>>
+    {
+        private readonly AppDbContext _context;
+
+        public GetApplicationByClientHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<ICollection<CreditApplyResponseDto>>> Handle(GetApplicationByClientQuery request, CancellationToken cancellationToken)
+        {
+            var creditAppliesByClient = await _context.CreditApplications
+                .Where(cp => cp.IdClient == request.IdClient)
+                .Select(cp => new CreditApplyResponseDto(cp.State, cp.CreateDate)) 
+                .ToListAsync();
+
+            if (creditAppliesByClient.Count == 0)
+            {
+                return Result<ICollection<CreditApplyResponseDto>>.Fail("Customer doesn't have any credit application");
+            }
+
+            return Result<ICollection<CreditApplyResponseDto>>.Ok(creditAppliesByClient);
+        }
+    }
+}
---
+++ CreditApplication.Application/UseCases/Queries/GetApplicationByClientQuery/GetApplicationByClientQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CreditApplication.Application.Common.Result;
+using CreditApplication.Application.Dtos;
+using MediatR;
+
+namespace CreditApplication.Application.UseCases.Queries.GetApplicationByClientQuery
+{
+    public class GetApplicationByClientQuery : IRequest<Result<ICollection<CreditApplyResponseDto>>>
+    {
+        public int IdClient { get; set; }
+    }
+}
